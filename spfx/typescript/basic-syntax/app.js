7afc250426889fc0b382c53b26adfadb643fc718
budostylz <lewis.shaun1@outlook.com>
2021-06-21 10:46:49 -0400

    commit

---
+++ spfx/typescript/basic-syntax/app.js
@@ -0,0 +1,10 @@
+"use strict";
+/*var message: string = "Hello World";
+console.log(message);*/
+class Greeting {
+    greet() {
+        console.log("Hello from TypeScript");
+    }
+}
+var obj = new Greeting();
+obj.greet();
---
+++ spfx/typescript/basic-syntax/app.ts
@@ -0,0 +1,11 @@
+/*var message: string = "Hello World";
+console.log(message);*/
+
+class Greeting {
+    greet(): void {
+        console.log("Hello from TypeScript")
+    }
+}
+
+var obj = new Greeting();
+obj.greet();
