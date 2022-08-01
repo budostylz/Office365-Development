70582a5b45afa696427ba9e218087dd34d8b7771
Shaun Lewis (AFFIRMA) <v-lewisshaun@microsoft.com>
2022-08-01 14:51:40 -0400

    'update'

--- migration/ImportantCommands.ps1
+++
@@ -1,8 +0,0 @@
-$keyword = "pcarlson"
-write-host $keyword
-SkySync-cli reports list --search $keyword --output-json
-$reports = SkySync-cli reports list --search $keyword --output-json | ConvertFrom-Json
-write-host $reports[0].id
-skysync-cli reports stats $reports[0].id
-
-
